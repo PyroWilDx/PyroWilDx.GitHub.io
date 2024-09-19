4d122bddca1554c0dd8d366aad1a6bf87cc14830
PyroWilDx <pyrowildx314.kqm@gmail.com>
2024-09-19 13:14:30 +0200

    Remove Duplicate Call

--- index.js
+++ index.js
@@ -1,76 +1,72 @@
-function exec() {
-    const linkMap = new Map([
-        ["C", "https://www.open-std.org/jtc1/sc22/wg14/"],
-        ["C++", "https://isocpp.org/"],
-        ["Java", "https://www.java.com/"],
-        ["TypeScript", "https://www.typescriptlang.org/"],
-        ["Python", "https://www.python.org/"],
-        ["Unreal Engine", "https://www.unrealengine.com/"],
-        ["Qt6", "https://www.qt.io/"],
-        ["OpenGL", "https://www.opengl.org/"],
-        ["SDL2", "https://www.libsdl.org/"],
-        ["Boost", "https://www.boost.org/"],
-        ["ThreeJs", "https://threejs.org/"],
-        ["AVX2", "https://www.intel.com/content/www/us/en/docs/intrinsics-guide/index.html#/"],
-        ["CMake", "https://cmake.org/"],
-        ["Make", "https://www.gnu.org/software/make/"],
-        ["Node", "https://nodejs.org/"],
-        ["Maven", "https://maven.apache.org/"],
-        ["Android Studio", "https://developer.android.com/studio/"],
-    ]);
+const linkMap = new Map([
+    ["C", "https://www.open-std.org/jtc1/sc22/wg14/"],
+    ["C++", "https://isocpp.org/"],
+    ["Java", "https://www.java.com/"],
+    ["TypeScript", "https://www.typescriptlang.org/"],
+    ["Python", "https://www.python.org/"],
+    ["Unreal Engine", "https://www.unrealengine.com/"],
+    ["Qt6", "https://www.qt.io/"],
+    ["OpenGL", "https://www.opengl.org/"],
+    ["SDL2", "https://www.libsdl.org/"],
+    ["Boost", "https://www.boost.org/"],
+    ["ThreeJs", "https://threejs.org/"],
+    ["AVX2", "https://www.intel.com/content/www/us/en/docs/intrinsics-guide/index.html#/"],
+    ["CMake", "https://cmake.org/"],
+    ["Make", "https://www.gnu.org/software/make/"],
+    ["Node", "https://nodejs.org/"],
+    ["Maven", "https://maven.apache.org/"],
+    ["Android Studio", "https://developer.android.com/studio/"],
+]);
 
-    const boxRects = document.querySelectorAll(".box-rect-content");
-    for (const boxRect of boxRects) {
-        if (linkMap.has(boxRect.textContent)) {
-            boxRect.href = linkMap.get(boxRect.textContent);
-            boxRect.target = "_blank";
-        }
+const boxRects = document.querySelectorAll(".box-rect-content");
+for (const boxRect of boxRects) {
+    if (linkMap.has(boxRect.textContent)) {
+        boxRect.href = linkMap.get(boxRect.textContent);
+        boxRect.target = "_blank";
     }
-
-    const colorPicker = document.getElementById("colorPicker");
-    colorPicker.addEventListener("input", () => {
-        const windowSelection = window.getSelection();
-        if (windowSelection) {
-            windowSelection.removeAllRanges();
-        }
-
-        const lightestMainColor = hslToRgb((colorPicker.value / 100) * 360, 100, 50);
-        const lightMainColor = [lightestMainColor[0] * (123 / 222),
-        lightestMainColor[1] * (93 / 161),
-        lightestMainColor[2] * (189 / 245)];
-        const midMainColor = [lightestMainColor[0] * (59 / 222),
-        lightestMainColor[1] * (43 / 161),
-        lightestMainColor[2] * (95 / 245)];
-        const darkMainColor = [lightestMainColor[0] * (33 / 222),
-        lightestMainColor[1] * (23 / 161),
-        lightestMainColor[2] * (56 / 245)];
-
-        document.documentElement.style.setProperty("--lightest-main-color", arrToRgb(lightestMainColor));
-        document.documentElement.style.setProperty("--light-main-color", arrToRgb(lightMainColor));
-        document.documentElement.style.setProperty("--mid-main-color", arrToRgb(midMainColor));
-        document.documentElement.style.setProperty("--dark-main-color", arrToRgb(darkMainColor));
-    });
-
-    const resetColorButton = document.getElementById("resetColorButton");
-    resetColorButton.addEventListener("click", () => {
-        colorPicker.value = 86;
-
-        document.documentElement.style.setProperty("--lightest-main-color", "#DEA1F5");
-        document.documentElement.style.setProperty("--light-main-color", "#7B5DBD");
-        document.documentElement.style.setProperty("--mid-main-color", "#3B2B5F");
-        document.documentElement.style.setProperty("--dark-main-color", "#211738");
-    });
-
-    const firstName = document.getElementById("firstName");
-    const lastName = document.getElementById("lastName");
-    setTimeout(() => {
-        typeText(firstName, "Pierre", () => {
-            typeText(lastName, "LIN");
-        });
-    }, 600);
 }
 
-exec();
+const colorPicker = document.getElementById("colorPicker");
+colorPicker.addEventListener("input", () => {
+    const windowSelection = window.getSelection();
+    if (windowSelection) {
+        windowSelection.removeAllRanges();
+    }
+
+    const lightestMainColor = hslToRgb((colorPicker.value / 100) * 360, 100, 50);
+    const lightMainColor = [lightestMainColor[0] * (123 / 222),
+    lightestMainColor[1] * (93 / 161),
+    lightestMainColor[2] * (189 / 245)];
+    const midMainColor = [lightestMainColor[0] * (59 / 222),
+    lightestMainColor[1] * (43 / 161),
+    lightestMainColor[2] * (95 / 245)];
+    const darkMainColor = [lightestMainColor[0] * (33 / 222),
+    lightestMainColor[1] * (23 / 161),
+    lightestMainColor[2] * (56 / 245)];
+
+    document.documentElement.style.setProperty("--lightest-main-color", arrToRgb(lightestMainColor));
+    document.documentElement.style.setProperty("--light-main-color", arrToRgb(lightMainColor));
+    document.documentElement.style.setProperty("--mid-main-color", arrToRgb(midMainColor));
+    document.documentElement.style.setProperty("--dark-main-color", arrToRgb(darkMainColor));
+});
+
+const resetColorButton = document.getElementById("resetColorButton");
+resetColorButton.addEventListener("click", () => {
+    colorPicker.value = 86;
+
+    document.documentElement.style.setProperty("--lightest-main-color", "#DEA1F5");
+    document.documentElement.style.setProperty("--light-main-color", "#7B5DBD");
+    document.documentElement.style.setProperty("--mid-main-color", "#3B2B5F");
+    document.documentElement.style.setProperty("--dark-main-color", "#211738");
+});
+
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+setTimeout(() => {
+    typeText(firstName, "Pierre", () => {
+        typeText(lastName, "LIN");
+    });
+}, 600);
 
 function hslToRgb(h, s, l) {
     s /= 100;
@@ -172,83 +168,6 @@ function typeText(el, text, callback) {
         callback();
     }
 }
-function exec() {
-    const color0 = generateRandomLightColor();
-    const { color1, color2 } = getTriadicColors(color0)
-    setConicGradient("intro-img-container", [color0, color1, color2]);
-
-    const linkMap = new Map([
-        ["C", "https://www.open-std.org/jtc1/sc22/wg14/"],
-        ["C++", "https://isocpp.org/"],
-        ["Java", "https://www.java.com/"],
-        ["TypeScript", "https://www.typescriptlang.org/"],
-        ["Python", "https://www.python.org/"],
-        ["Unreal Engine", "https://www.unrealengine.com/"],
-        ["Qt6", "https://www.qt.io/"],
-        ["OpenGL", "https://www.opengl.org/"],
-        ["SDL2", "https://www.libsdl.org/"],
-        ["Boost", "https://www.boost.org/"],
-        ["ThreeJs", "https://threejs.org/"],
-        ["AVX2", "https://www.intel.com/content/www/us/en/docs/intrinsics-guide/index.html#/"],
-        ["CMake", "https://cmake.org/"],
-        ["Make", "https://www.gnu.org/software/make/"],
-        ["Node", "https://nodejs.org/"],
-        ["Maven", "https://maven.apache.org/"],
-        ["Android Studio", "https://developer.android.com/studio/"],
-    ]);
-
-    const boxRects = document.querySelectorAll(".box-rect-content");
-    for (const boxRect of boxRects) {
-        if (linkMap.has(boxRect.textContent)) {
-            boxRect.href = linkMap.get(boxRect.textContent);
-            boxRect.target = "_blank";
-        }
-    }
-
-    const colorPicker = document.getElementById("colorPicker");
-    colorPicker.addEventListener("input", () => {
-        const windowSelection = window.getSelection();
-        if (windowSelection) {
-            windowSelection.removeAllRanges();
-        }
-
-        const lightestMainColor = hslToRgb((colorPicker.value / 100) * 360, 100, 50);
-        const lightMainColor = [lightestMainColor[0] * (123 / 222),
-        lightestMainColor[1] * (93 / 161),
-        lightestMainColor[2] * (189 / 245)];
-        const midMainColor = [lightestMainColor[0] * (59 / 222),
-        lightestMainColor[1] * (43 / 161),
-        lightestMainColor[2] * (95 / 245)];
-        const darkMainColor = [lightestMainColor[0] * (33 / 222),
-        lightestMainColor[1] * (23 / 161),
-        lightestMainColor[2] * (56 / 245)];
-
-        document.documentElement.style.setProperty("--lightest-main-color", arrToRgb(lightestMainColor));
-        document.documentElement.style.setProperty("--light-main-color", arrToRgb(lightMainColor));
-        document.documentElement.style.setProperty("--mid-main-color", arrToRgb(midMainColor));
-        document.documentElement.style.setProperty("--dark-main-color", arrToRgb(darkMainColor));
-    });
-
-    const resetColorButton = document.getElementById("resetColorButton");
-    resetColorButton.addEventListener("click", () => {
-        colorPicker.value = 86;
-
-        document.documentElement.style.setProperty("--lightest-main-color", "#DEA1F5");
-        document.documentElement.style.setProperty("--light-main-color", "#7B5DBD");
-        document.documentElement.style.setProperty("--mid-main-color", "#3B2B5F");
-        document.documentElement.style.setProperty("--dark-main-color", "#211738");
-    });
-
-    const firstName = document.getElementById("firstName");
-    const lastName = document.getElementById("lastName");
-    setTimeout(() => {
-        typeText(firstName, "Pierre", () => {
-            typeText(lastName, "LIN");
-        });
-    }, 600);
-}
-
-exec();
 
 function hslToRgb(h, s, l) {
     s /= 100;
